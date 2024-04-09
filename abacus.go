00289aecba9fd226ad5fbd58b57a200780011838
Welcome <welcome@Welcomes-MacBook-Pro.local>
2024-04-09 12:05:51 +0300

    fix: Abacus

--- abacus.go
+++ abacus.go
@@ -1,18 +1,9 @@
 package sprint
 
-import "fmt"
-
 func Abacus(a int, b int) int {
-
 	if b == 0 {
 		return 0
 	}
 	c := a / b
 	return c
-}
-
-func main() {
-
-	c := Abacus(4, 8)
-	fmt.Println("c", c)
 }
