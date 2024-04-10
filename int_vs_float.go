22ff69bd94314e9070617f5deacd069ea88aafd2
Welcome <welcome@Welcomes-MacBook-Pro.local>
2024-04-10 10:15:39 +0300

    fix: int vs float

--- int_vs_float.go
+++ int_vs_float.go
@@ -3,9 +3,9 @@ package sprint
 func IntVsFloat(i int, f float32) string {
 	if float32(i) > f {
 		return "Integer"
+	} else if f > float32(i) {
+		return "Float"
+	} else {
+		return "Same"
 	}
-	if float32(i) == f {
-		return "same"
-	}
-	return "Float"
 }
