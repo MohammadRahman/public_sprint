50ce18314e63a3cf4eecf8a2864573441e678e5d
Welcome <welcome@Welcomes-MacBook-Pro.local>
2024-04-10 10:39:50 +0300

    the provided month falls in season

---
+++ season.go
@@ -0,0 +1,19 @@
+package sprint
+
+import "fmt"
+
+func Season(monthName string) string {
+
+	switch monthName {
+	case "dec", "jan", "feb":
+		return "winter"
+	case "mar", "apr", "may":
+		return "spring"
+	case "jun", "jul", "aug":
+		return "summer"
+	case "sep", "oct", "nov":
+		return "autumn"
+	default:
+		return fmt.Sprintf("Invalid input: %s", monthName)
+	}
+}
