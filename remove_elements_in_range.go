90e1feac14773e4e10e4c5a119a42d464e4c10e1
Welcome <welcome@Welcomes-MacBook-Pro.local>
2024-04-15 12:19:25 +0300

    fix and works

--- remove_elements_in_range.go
+++ remove_elements_in_range.go
@@ -1,30 +1,24 @@
 package sprint
 
 func RemoveElementsInRange(arr []float64, from, to int) []float64 {
-	// Normalize negative indices
-	if from < 0 {
-		from = len(arr) + from
-	}
-	if to < 0 {
-		to = len(arr) + to
-	}
 
+	switch {
+	case from < 0:
+		from = 0
+	case from >= len(arr):
+		from = len(arr)
+	}
+	switch {
+	case to < 0:
+		to = 0
+	case to >= len(arr):
+		to = len(arr)
+	}
 	if from > to {
 		from, to = to, from
 	}
-	if from < 0 {
-		from = 0
+	if from < 0 && to < 0 {
+		return arr
 	}
-	if to >= len(arr) {
-		to = len(arr) - 1
-	}
-	var result []float64
-	if from > 0 {
-		result = append(result, arr[:from]...)
-	}
-	if to > 0 {
-		result = append(result, arr[to:]...)
-	}
-
-	return result
+	return append(arr[:from], arr[to:]...)
 }
