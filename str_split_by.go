e78a988f12aee6e917252bfdcd0f9afb346ec37a
Welcome <welcome@Welcomes-MacBook-Pro.local>
2024-04-17 16:33:57 +0300

    str_split_by

---
+++ str_split_by.go
@@ -0,0 +1,30 @@
+package sprint
+
+func StrSplitBy(s, sep string) []string {
+	var substrings []string
+	substring := ""
+
+	for i := 0; i < len(s); i++ {
+		match := true
+		for j := 0; j < len(sep); j++ {
+			if i+j >= len(s) || s[i+j] != sep[j] {
+				match = false
+				break
+			}
+		}
+
+		if match {
+			substrings = append(substrings, substring)
+			substring = ""
+			i += len(sep) - 1
+		} else {
+			substring += string(s[i])
+		}
+	}
+
+	if len(substring) > 0 {
+		substrings = append(substrings, substring)
+	}
+
+	return substrings
+}
