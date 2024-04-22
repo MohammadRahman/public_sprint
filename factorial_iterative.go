0f245fb9344f08a2488b21b2f9d4ee749d036c67
Welcome <welcome@Welcomes-MacBook-Pro.local>
2024-04-22 10:27:50 +0300

    factorial_iterative

---
+++ factorial_iterative.go
@@ -0,0 +1,22 @@
+package sprint
+
+import "math"
+
+func FactorialIterative(n int) int {
+	// Check for invalid input
+	if n < 0 {
+		return 0
+	}
+
+	result := 1
+
+	// Compute factorial iteratively
+	for i := 1; i <= n; i++ {
+		if result > math.MaxInt32/i {
+			return 0
+		}
+		result *= i
+	}
+
+	return result
+}
