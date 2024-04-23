d08792c703a9816da9066c78a30eb92b152bc2bc
Welcome <welcome@Welcomes-MacBook-Pro.local>
2024-04-23 12:09:14 +0300

    fix: arr_any_108

--- arr_any.go
+++ arr_any.go
@@ -11,17 +11,33 @@ func ArrAny(f func(string) bool, a []string) bool {
 
 func IsLower(s string) bool {
 	for _, char := range s {
-		if char >= 'a' || char <= 'z' {
-			return true
+		if char < 'a' || char > 'z' {
+			return false
 		}
 	}
-	return false
+	return true
 }
 func IsNumeric(s string) bool {
 	for _, char := range s {
-		if char >= '0' || char <= '9' {
-			return true
+		if char < '0' || char > '9' {
+			return false
 		}
 	}
-	return false
+	return true
+}
+func IsUpper(s string) bool {
+	for _, char := range s {
+		if char < 'A' || char > 'Z' {
+			return false
+		}
+	}
+	return true
+}
+func IsAlphanumeric(s string) bool {
+	for _, char := range s {
+		if IsUpper(string(char)) || IsLower(string(char)) || IsNumeric(string(char)) {
+			return false
+		}
+	}
+	return true
 }
