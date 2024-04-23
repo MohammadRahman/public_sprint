608135401c5697df8993406078299238344c707c
Welcome <welcome@Welcomes-MacBook-Pro.local>
2024-04-23 10:46:10 +0300

    fix: arr_any

--- arr_any.go
+++ arr_any.go
@@ -1,27 +1,28 @@
 package sprint
 
-// ch == 65 - 87
-func isUpper(s string) bool {
-	for _, ch := range s {
-		if ch >= 'A' || ch <= 'Z' {
-			return true
-		}
-	}
-	return false
-}
-func isAlphaNumeric(s string) bool {
-	for _, ch := range s {
-		if (isUpper(string(ch)) || ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') {
-			return true
-		}
-	}
-	return false
-}
 func ArrAny(f func(string) bool, a []string) bool {
-	for _, ch := range a {
-		if f(ch) {
+	for _, str := range a {
+		if f(str) {
 			return true
 		}
 	}
 	return false
+}
+func IsUpper(s string) bool {
+	for _, char := range s {
+		if 'A' <= char && char <= 'Z' {
+			continue
+		}
+		return false
+	}
+	return true
+}
+func IsAlphanumeric(s string) bool {
+	for _, char := range s {
+		if ('a' <= char && char <= 'z') || ('A' <= char && char <= 'Z') || ('0' <= char && char <= '9') {
+			continue
+		}
+		return false
+	}
+	return true
 }
