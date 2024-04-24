eecb3992a5f5659d54f1892ba67693c4d3c9b400
Welcome <welcome@Welcomes-MacBook-Pro.local>
2024-04-24 05:33:47 +0300

    fix: ups_324

--- is_sorted.go
+++ is_sorted.go
@@ -1,19 +1,5 @@
 package sprint
 
-func IsSorted(f func(a, b string) int, arr []string) bool {
-	n := len(arr)
-	if n <= 1 {
-		return true
-	}
-
-	for i := 1; i < n; i++ {
-		if f(arr[i-1], arr[i]) > 0 {
-			return false
-		}
-	}
-
-	return true
-}
 func StrCompare(a, b string) int {
 	if a > b {
 		return 1
@@ -21,4 +7,28 @@ func StrCompare(a, b string) int {
 		return -1
 	}
 	return 0
+}
+
+func IsSorted(f func(a, b string) int, arr []string) bool {
+	n := len(arr)
+	if n <= 1 {
+		return true
+	}
+
+	ascending := f(arr[0], arr[1]) <= 0
+	for i := 1; i < n; i++ {
+		if f(arr[i-1], arr[i]) == 0 {
+			continue
+		}
+		if ascending {
+			if f(arr[i-1], arr[i]) > 0 {
+				return false
+			}
+		} else {
+			if f(arr[i-1], arr[i]) < 0 {
+				return false
+			}
+		}
+	}
+	return true
 }
