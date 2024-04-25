60c3390bc7a7b7e16370794a62ff1d7dde34d1a6
Welcome <welcome@Welcomes-MacBook-Pro.local>
2024-04-25 11:45:22 +0300

    fix: overlap_5

--- overlap.go
+++ overlap.go
@@ -4,10 +4,12 @@ func Overlap(arr1, arr2 []int) []int {
 	if len(arr1) == 0 || len(arr2) == 0 {
 		return []int{}
 	}
+
 	count := make(map[int]int)
 	for _, num := range arr1 {
 		count[num]++
 	}
+
 	var result []int
 	for _, num := range arr2 {
 		if count[num] > 0 {
@@ -15,14 +17,5 @@ func Overlap(arr1, arr2 []int) []int {
 			count[num]--
 		}
 	}
-
-	for i := 0; i < len(result)-1; i++ {
-		for j := 0; j < len(result)-1-i; j++ {
-			if result[j] > result[j+1] {
-				result[j], result[j+1] = result[j+1], result[j]
-			}
-		}
-	}
-
 	return result
 }
