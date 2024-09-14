cc35d51c31749693532e415aefbfa0642fa7e2e7
mohammadrahman <obaidurapu12@gmail.com>
2024-09-14 22:25:20 +0600

    FloatMinusInt

---
+++ FloatMinusInt.java
@@ -0,0 +1,10 @@
+package sprint;
+
+public class FloatMinusInt {
+    public double subtractIntFromDoubleAndReturnDouble(double x, int y){
+        return (x - y);
+    }
+    public int subtractIntFromDoubleAndReturnInt(double x, int y){
+        return (int) (x - y);
+    }
+}
