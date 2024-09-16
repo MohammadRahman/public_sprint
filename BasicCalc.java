ba155a9742ba849c1ea1f313c7f7297fb562db4a
mohammadrahman <obaidurapu12@gmail.com>
2024-09-16 23:39:31 +0600

    fix: do arithmatic operations

--- BasicCalc.java
+++ BasicCalc.java
@@ -2,7 +2,7 @@ package sprint;
 
 public class BasicCalc {
     public int doOperation(int a, char op, int b) {
-       if(a == 0 || b == 0 && op == '%' || op == '*' || op == '/'){
+       if(a == '0' || b == '0' && op == '%' || op == '*' || op == '/'){
         return 0;
        }else if(op == '+'){
         return a + b;
