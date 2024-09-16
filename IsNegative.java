8c2cb920a1383dc5cf189fdf8f9377c4f5945583
mohammadrahman <obaidurapu12@gmail.com>
2024-09-16 23:09:46 +0600

    checksIfNegative

---
+++ IsNegative.java
@@ -0,0 +1,10 @@
+package sprint;
+
+public class IsNegative {
+    public boolean checkIfNegative(int num){
+        if (num < 0) {
+            return true;
+        }
+        return false;
+    }
+}
