4948425b10ed67560f909168679145dccf69c1b8
mohammadrahman <obaidurapu12@gmail.com>
2024-09-16 23:20:22 +0600

    whichIsGreater

---
+++ NumberComparator.java
@@ -0,0 +1,13 @@
+package sprint;
+
+public class NumberComparator {
+    public String whichIsGreater(int n, double f) {
+        if(n > f){
+            return "Integer";
+        }else if(n < f){
+            return "Float";
+        }else{
+            return "Equal";
+        }
+    }
+}
