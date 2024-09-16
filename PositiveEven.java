631c6674504dc85ddd718d5f83c1e9c8d6479c04
mohammadrahman <obaidurapu12@gmail.com>
2024-09-16 23:15:58 +0600

    isPositiveAndEven

---
+++ PositiveEven.java
@@ -0,0 +1,10 @@
+package sprint;
+
+public class PositiveEven {
+    public boolean isPositiveAndEven(int num){
+        if(num > 0 && num%2 == 0){
+            return true;
+        }
+        return false;
+    }
+}
