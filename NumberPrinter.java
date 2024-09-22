27c8c5d901ebbf9ba02a320036e565f378e3bf5a
mohammadrahman <obaidurapu12@gmail.com>
2024-09-22 23:55:29 +0600

    fix: number printer

--- NumberPrinter.java
+++ NumberPrinter.java
@@ -3,7 +3,7 @@ package sprint;
 public class NumberPrinter {
     public void printNums(int num){
         if(num >= 0){
-            for(int i = 0; i < num; i++){
+            for(int i = 0; i <= num; i++){
                 System.out.println(i);
             }
         }else{
