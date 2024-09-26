910b712b04bce2efba140722d387920baa9ca602
mohammadrahman <obaidurapu12@gmail.com>
2024-09-26 23:58:08 +0600

    fix: ListManipulator

--- ListManipulator.java
+++ ListManipulator.java
@@ -9,7 +9,11 @@ public class ListManipulator {
             return list;
         }
         list.remove(list.size() -1);
-        list.set(list.size() -1, "The size of the list is " + list.size());
+        if (list.isEmpty()) {
+            list.add("The size of the list is 1");
+        }else{
+            list.set(list.size() -1, "The size of the list is " + list.size());
+        }
         list.add("last");
         list.set(0,"first");
         return list;
