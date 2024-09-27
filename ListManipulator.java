0f9e5be1623a0c59360a84155099ae59509cbd2b
mohammadrahman <obaidurapu12@gmail.com>
2024-09-27 10:08:11 +0600

    fix: ListManipulator

--- ListManipulator.java
+++ ListManipulator.java
@@ -6,21 +6,11 @@ public class ListManipulator {
 
     public List<String> manipulateList(List<String> list) {
         // Handle the empty list case.
-        if (list == null || list.isEmpty()) {
-            list.add("first");
-            list.add("The size of the list is 1");
-            list.add("last");
-            return list;
-        }
-
-        list.remove(list.size() - 1);
-
-        list.add("The size of the list is " + list.size());
-
+        list.remove(list.size() -1);
+        list.set(list.size() -1, "The size of the list is " + list.size());
         list.add("last");
-
         list.set(0, "first");
-
+        
         return list;
     }
 }
