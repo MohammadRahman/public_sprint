dbeb6c399ab0dc7d9dd4b78aaed7fc1dde1e408f
mohammadrahman <obaidurapu12@gmail.com>
2024-10-06 21:29:13 +0300

    DayChecker

---
+++ DayChecker.java
@@ -0,0 +1,20 @@
+package sprint;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class DayChecker {
+    public String checkDayType(LocalDate date) {
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+
+        switch (dayOfWeek) {
+            case SATURDAY:
+            case SUNDAY:
+                return "Weekend";
+            case WEDNESDAY:
+                return "Hump Day!";          
+            default:
+                return "Weekday";
+        }
+    }
+}
