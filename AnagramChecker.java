d8ca1c4a69037e5dcff030d8d0ee220fab4029d6
mohammadrahman <obaidurapu12@gmail.com>
2024-10-08 00:14:04 +0300

    fix: empty string issue in AnagramChecker

--- AnagramChecker.java
+++ AnagramChecker.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 
 public class AnagramChecker {
     public static boolean areAnagrams(String str1, String str2) {
-        if(str1.isEmpty() || str2.isEmpty()){
+        if(str1 == null || str2 == null){
             return false;
         }
+        if (str1.isEmpty() && str2.isEmpty()) {
+            return true;
+        }
+        
         String formatedStr1 = str1.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
        String formatedStr2 = str2.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
