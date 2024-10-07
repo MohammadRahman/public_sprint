5dd5bfa018dd484caaa501616197ba2a505bba51
mohammadrahman <obaidurapu12@gmail.com>
2024-10-08 00:10:18 +0300

    AnagramChecker

---
+++ AnagramChecker.java
@@ -0,0 +1,25 @@
+package sprint;
+
+import java.util.Arrays;
+
+public class AnagramChecker {
+    public static boolean areAnagrams(String str1, String str2) {
+        if(str1.isEmpty() || str2.isEmpty()){
+            return false;
+        }
+        String formatedStr1 = str1.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+       String formatedStr2 = str2.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+       if(formatedStr1.length() != formatedStr2.length()){
+        return false;
+       }
+       char[] charArr1 = formatedStr1.toCharArray();
+       char[] charArr2 = formatedStr2.toCharArray();
+
+       Arrays.sort(charArr1);
+       Arrays.sort(charArr2);
+
+       return Arrays.equals(charArr1, charArr2);
+
+    }
+}
