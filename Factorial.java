15d31c2ff1f5a1a45346c7de0ba606c2c5197a18
mohammadrahman <obaidurapu12@gmail.com>
2024-10-15 08:56:26 +0300

    fix: Factorial number

--- Factorial.java
+++ Factorial.java
@@ -1,11 +1,16 @@
+package sprint;
+
 public class Factorial {
-    public static int calculateFactorial(int num){
-        if(num < 0){
+
+    public static int calculateFactorial(int n) {
+        // solution code here
+        if(n < 0){
             return 0;
         }
-        if(num ==0 || num == 1){
+        if(n == 0 || n == 1){
             return 1;
         }
-        return num* calculateFactorial(num -1);
+        return n * calculateFactorial(n -1);
     }
+
 }
